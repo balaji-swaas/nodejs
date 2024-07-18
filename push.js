1e8bd080fcf90296b7cc129cc9fe33cef6429903
balaji-swaas <balaji.manjunath@swaas.net>
2024-07-18 17:41:46 +0530

    Add files via upload

---
+++ push.js
@@ -0,0 +1,38 @@
+//Problem-4: To-do List
+
+/* Logic Implementation Function */
+function addTask(task) {
+    res=["Buy groceries","Clean the house","Finish today homework","Study for Annual Exam","Go to Sleep"];
+    return res;
+}
+
+
+function displayTasks() {
+    console.log("Today Activities List:");
+    /* Write your logic here --- Start */
+    result=res;
+    console.log(result);
+    /* Write your logic here --- End */
+}
+/* Logic Implementation Function */
+/* Function Call */
+/* Inputs */
+addTask("Buy groceries");
+addTask("Clean the house");
+addTask("Finish today homework");
+addTask("Study for Annual Exam");
+addTask("Go to Sleep");
+/* Inputs */
+
+displayTasks();
+/* Function Call */
+
+
+/* Expected Output */
+// Today Activities List:
+// 1. Buy groceries
+// 2. Clean the house
+// 3. Finish today's homework
+// 4. Study for Annual Exam
+// 5. Go to Sleep
+/* Expected Output */
